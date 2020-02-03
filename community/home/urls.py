cbed51f69179b789168572e6a55b90b487169386
guohaoyu110 <guohaoyu110@gmail.com>
2020-02-03 00:45:19 -0800

    add templates

---
+++ community/home/urls.py
@@ -0,0 +1,8 @@
+# create a route.
+from django.urls import path
+
+from .views import home
+
+urlpatterns = [
+    path('', home, name='home'),
+]
--- community/home/views.py
+++ community/home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from django.shortcuts import render
+
+
+def home(request):
+    """
+    homepage
+    """
+    return render(request, 'home/home.html')
